cc7ba564efbaa71b10761ff20b10742e72d75ca9
NelsonNV <nelsonluisnv2@gmail.com>
2024-05-23 17:38:58 -0400

    feat(api): create controller for POST requests of get_usuario_status_passwords

---
+++ controller/seguridadPassword.php
@@ -0,0 +1,18 @@
+<?php
+require_once("../config/conexion.php");
+require_once("../models/SeguridadPassword.php");
+require_once("../models/RegistroLog.php");
+if (isset($_SESSION["usu_id"]) && ($_SESSION["usu_tipo"] == 1 || $_SESSION["usu_tipo"] == 2)) {
+
+$RegistroLog= new RegistroLog();
+
+if (isset($_POST["op"])) {
+    switch ($_POST["op"]) {
+        case "password_status":
+            $passSeg = new SeguridadPassword;
+            $response = $passSeg->get_usuarios_status_passwords(); 
+            echo json_encode($response);
+        break;
+        }
+    }
+}
