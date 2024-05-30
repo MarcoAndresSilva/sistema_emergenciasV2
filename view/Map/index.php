8faaef17d4ad8ee9d476f255893bff11eea6c8ce
marcosilva1088 <marco.silva@munimelipilla.cl>
2024-05-30 12:13:06 -0400

    Modify view/Map/index.php: description: add script to botton of file js to be able google map API_KEY

--- view/Map/index.php
+++ view/Map/index.php
@@ -1,8 +1,9 @@
 <?php
 require_once("../../config/conexion.php");
 require_once("../MainJs/js.php");
+
+// Verificar la sesión del usuario
 if (isset($_SESSION["usu_id"]) && ($_SESSION["usu_tipo"] == 1 || $_SESSION["usu_tipo"] == 2)) {
-	
 ?>
 
 <!DOCTYPE html>
@@ -22,46 +23,51 @@ if (isset($_SESSION["usu_id"]) && ($_SESSION["usu_tipo"] == 1 || $_SESSION["usu_
 
     <div class="page-content">
         <div class="container-fluid">
-        <header class="section-header">
-				<div class="tbl">
-					<div class="tbl-row">
-						<div class="tbl-cell">
-							<h3>Mapa</h3>
-							<ol class="breadcrumb breadcrumb-simple">
-								<li><a href="#">Mapa</a></li>
-								<li class="active">Mapa</li>
-							</ol>
-						</div>
-					</div>
-				</div>
-			</header>
+            <header class="section-header">
+                <div class="tbl">
+                    <div class="tbl-row">
+                        <div class="tbl-cell">
+                            <h3>Mapa</h3>
+                            <ol class="breadcrumb breadcrumb-simple">
+                                <li><a href="#">Mapa</a></li>
+                                <li class="active">Mapa</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </header>
 
-			<div id="map"></div>
-			<div id="contenedor-imagen">
-				<img src="" alt="" id="imagen-cargada">
-			</div>
-
+            <div id="map"></div>
+            <div id="contenedor-imagen">
+                <img src="" alt="" id="imagen-cargada">
+            </div>
 
         </div><!--.container-fluid-->
     </div><!--.page-content-->
-	<script type="text/javascript" src="map.js"></script>
-	<script>
-		document.getElementById('show-hide-sidebar-toggle').addEventListener('click', function(e) {
-			e.preventDefault();
+    
+    <script type="text/javascript" src="map.js"></script>
+    <script
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAQrYCFSz7Q-a-WONxo4yymu9SAPgmaA6c&callback=initMap&v=weekly&libraries=marker"
+        defer
+    ></script>
+    <script>
+        document.getElementById('show-hide-sidebar-toggle').addEventListener('click', function(e) {
+            e.preventDefault();
 
-			var body = document.body;
+            var body = document.body;
 
-			if (!body.classList.contains('sidebar-hidden')) {
-				body.classList.add('sidebar-hidden');
-			} else {
-				body.classList.remove('sidebar-hidden');
-			}
-		});
-	</script>
+            if (!body.classList.contains('sidebar-hidden')) {
+                body.classList.add('sidebar-hidden');
+            } else {
+                body.classList.remove('sidebar-hidden');
+            }
+        });
+    </script>
 </body>
+
 <?php
-}else{
-	header("location:".Conectar::ruta()."index.php");
+} else {
+    header("location:".Conectar::ruta()."index.php");
 }
 ?>
 
