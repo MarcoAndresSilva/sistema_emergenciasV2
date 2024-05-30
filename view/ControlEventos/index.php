b93fe83e07ceef6034c804073ddfc8e8d7c0d296
marcosilva1088 <marco.silva@munimelipilla.cl>
2024-05-30 12:09:15 -0400

    Modificar view/ControlEventos/index.php: description: feat(frontEnd) add new panel to close events

--- view/ControlEventos/index.php
+++ view/ControlEventos/index.php
@@ -50,6 +50,7 @@ if (isset($_SESSION["usu_id"]) && ($_SESSION["usu_tipo"] == 1 || $_SESSION["usu_
                             <th style="width:10%">Estado</th>
                             <th style="width:15%">Hora Apertura</th>
                             <th style="width:5%">Derivar</th>
+							<th style="width:5%">Cerrar</th>
 						</tr>
 					</thead>
 					<tbody id="datos-criticos">
@@ -75,7 +76,7 @@ if (isset($_SESSION["usu_id"]) && ($_SESSION["usu_tipo"] == 1 || $_SESSION["usu_
 				</div>
 				<div id="distribucion-box">
 					<div id="distribucion-espacios">
-						<h3 class="derivacion-title">Panel de Derivaciones</h3>
+						<h3 class="derivacion-title">Panel de Derivación</h3>
 						<fieldset class="form-group" id="grupito">
 							<label class="form-label bold" for="id_nivel_peligro" id="asignaciones-title">Id de Evento</label>
 							<div id="ev_id"></div>
@@ -102,29 +103,75 @@ if (isset($_SESSION["usu_id"]) && ($_SESSION["usu_tipo"] == 1 || $_SESSION["usu_
 						<fieldset class="form-group box-selection" id="buttons-group">
 							<label class="form-label semibold" id="asignaciones-title" for="id_nivel_peligro">Seleccionar</label>
 							<div class="btn-box">
-								<button id="btn"type='button' class='btn btn-inline btn-success btn-sm ladda-button btnActualizarTodos'> Actualizar
+								<button id="btn"type='button' class='btn btn-inline btn-success btn-sm ladda-button btnActualizarTodos'> Derivar
 									<span><i class="fa-solid fa-arrow-up-from-bracket"></i> </span>
 								</button>		
 							</div>
-							<div class="btn-box">
+							<!-- <div class="btn-box">
 								<button id="btn"type='button' class='btn btn-inline btn-warning btn-sm ladda-button btnCerrarEvento close-btn'>Cerrar Evento <span> <i class="fa-regular fa-circle-xmark"></i></span>
 					 			</button>
-							</div>
+							</div> -->
+
 						</fieldset>	
 					</div>
 				</div>
 			</div>
+
 			<div id="modal-mapa" class="modal-overlay">
                 <div class="vista-mapa" id="map">
                 </div>
                 <button id='btn' type='button' class='btn btn-inline btn-primary btn-sm ladda-button btnCrearRuta' > Ir </button>
                 <span class="glyphicon glyphicon-remove CerrarModalMap"></span>
             </div>
-			
-        </div><!--.container-fluid-->
-    </div><!--.page-content-->
 
-	<?php require_once("../MainFooter/footer.php"); ?>
+			<div id="selector-cerrar" class="modal-overlay modalBox" >
+				<div class="btn-cancelar">
+					<button id="btn"type='button' class='btn btn-inline btn-danger btn-sm ladda-button btnCancelarCerrar'> <i class='fa fa-close'></i>
+					</button>
+				</div>
+				<div id="distribucion-box">
+					<div id="distribucion-espacios">
+						<h3 class="derivacion-title">Panel de Cierre</h3>
+
+						<fieldset class="form-group" id="grupito">
+							<label class="form-label bold" id="asignaciones-title">Id de Evento</label>
+							<div id="ev_id_cierre"></div>
+						</fieldset>
+						<fieldset class="form-group" id="grupito">
+							<label class="form-label bold" id="asignaciones-title">Categoria</label>
+							<div id="cat_nom_cierre"></div>
+						</fieldset>
+						<fieldset class="form-group" id="grupito">
+							<label class="form-label bold" id="asignaciones-title">Nombre Apellido</label>
+							<input type="text" class="form-control" id="nombre_apellido" placeholder="Nombre y Apellido" value="<?php echo $_SESSION['usu_nom'] . ' ' . $_SESSION['usu_ape']; ?>" readonly>
+
+						</fieldset>
+						<fieldset class="form-group" id="grupito">
+							<label class="form-label bold" id="asignaciones-title">Detalle del Cierre</label>
+							<input type="text" class="form-control" id="detalle_cierre" name="detalle_cierre" placeholder="Detalle del cierre">
+						</fieldset>
+						<fieldset class="form-group">
+							<label class="form-label bold" id="asignaciones-title">Seleccione el motivo del cierre</label>
+							<select id="motivo_cierre" class="form-control">
+								<!-- Datos de consulta -->
+							</select>
+						</fieldset>
+
+						<fieldset class="form-group box-selection" id="buttons-group">
+							<label class="form-label semibold" id="asignaciones-title">Seleccionar</label>
+							<div class="btn-box">
+								<button id="btn" type="button" class="btn btn-inline btn-warning btn-sm ladda-button btnCerrarEvento close-btn">
+									Cerrar Evento <span><i class="fa-regular fa-circle-xmark"></i></span>
+								</button>
+							</div>
+						</fieldset>
+					</div>
+				</div>
+			</div>
+		</div>
+   	</div><!--.page-content-->
+
+<?php require_once("../MainFooter/footer.php"); ?>
 
 </body>
 
