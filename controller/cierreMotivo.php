f62c3fad809aa1e9a63012d9fdaec7e4327332dc
nelson <nelsonluisnv2@gmail.com>
2024-06-04 16:20:30 -0400

    feat(api):basic controller add-on
    crud requests are mode with the operations that are as "op" and
    depending on the case the data is received as post to execute the
    operation

---
+++ controller/cierreMotivo.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once("../config/conexion.php");
+require_once '../models/CierreMotivo.php';
+
+$model = new CierreMotivo();
+
+if (isset($_SESSION["usu_id"]) && ($_SESSION["usu_tipo"] == 1 || $_SESSION["usu_tipo"] == 2)) {
+
+if (isset($_GET["op"])) {
+    switch ($_GET["op"]) {
+        case 'add_cierre_motivo':
+            $motivo = $_POST['motivo'];
+            $result = $model->add_motivo_cierre($motivo);
+            echo json_encode($result);
+            break;
+
+        case 'update_cierre_motivo':
+            $motivo = $_POST['motivo'];
+            $id_mov = $_POST['id_mov'];
+            $result = $model->update_motivo_cierre($motivo, $id_mov);
+            echo json_encode($result);
+            break;
+
+        case 'delete_cierre_motivo':
+            $id_mov = $_POST['id_mov'];
+            $result = $model->delete_motivo_cierre($id_mov);
+            echo json_encode($result);
+            break;
+
+        case 'get_cierre_motivo':
+            $result = $model->get_motivo_cierre();
+            echo json_encode($result);
+            break;
+
+        default:
+            echo json_encode(['error' => 'Operación no reconocida']);
+            break;
+    }
+}
+}
