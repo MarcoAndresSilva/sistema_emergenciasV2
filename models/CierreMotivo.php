08d65945109cf966334a7e43d5f35776cbbb5543
nelson <nelsonluisnv2@gmail.com>
2024-06-06 10:55:59 -0400

    refactor: data validation and inprovement of status responses
    added checks that the data are the corresponding values or type values
    and improved return for status and error messages, success and warning.

--- models/CierreMotivo.php
+++ models/CierreMotivo.php
@@ -36,18 +36,34 @@ class CierreMotivo extends Conectar {
         }
         return $response;
     }
-    function update_motivo_cierre($motivo,$id_mov){ 
+
+    function update_motivo_cierre($motivo, $mov_id) { 
+        // Validar que $mov_id sea un número
+        if (!is_numeric($mov_id)) {
+            return array('status' => 'warning', 'message' => " ID del motivo debe ser un número, ID recibida: $mov_id.");
+        }
+    
+        // Validar que $motivo sea una cadena de texto
+        if (!is_string($motivo)) {
+            return array('status' => 'warning', 'message' => 'El motivo debe ser un texto.');
+        }
+    
         $conexion = parent::Conexion();
-        $sql = "UPDATE tm_cierre_motivo SET motivo:motivo WHERE id_mov=:id_mov";
+        $sql = "UPDATE tm_cierre_motivo SET motivo = :motivo WHERE mov_id = :mov_id";
         $query = $conexion->prepare($sql);
-        $query->bindParam(':motivo',$motivo);
-        $query->bindParam(':id_mov',$id_mov);
-        $query->execute();
-        $resultado = $query->fetch();
-        if (is_array($resultado) and count($resultado) > 0) {
-            return true;
-        }elseif(is_array($resultado) and count($resultado) == 0){
-            return false;
+        $query->bindParam(':motivo', $motivo);
+        $query->bindParam(':mov_id', $mov_id);
+    
+        try {
+            $query->execute();
+            // Verificar si se realizó la actualización correctamente
+            if ($query->rowCount() > 0) {
+                return array('status' => 'success', 'message' => 'Motivo actualizado correctamente.');
+            } else {
+                return array('status' => 'warning', 'message' => 'No se encontró ningún registro para actualizar.');
+            }
+        } catch (PDOException $e) {
+            return array('status' => 'error', 'message' => 'Error al ejecutar la consulta: ' . $e->getMessage());
         }
     }
 function delete_motivo_cierre($id_mov){ 
