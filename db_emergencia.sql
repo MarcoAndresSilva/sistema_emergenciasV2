2cec0c5cd4b1056a395dd7df7fe9eca75885d3d3
nelson <nelsonluisnv2@gmail.com>
2024-06-11 16:50:45 -0400

    refactor(db): change to varchar to int for FK
    now the reason data is a FK to be related to the table tm_cierre_motivo

--- db_emergencia.sql
+++ db_emergencia.sql
@@ -620,7 +620,7 @@ CREATE TABLE `tm_ev_cierre` (
   `usu_id` int(11) NOT NULL,
   `ev_id` int(11) NOT NULL,
   `detalle` varchar(300) NOT NULL,
-  `motivo` varchar(300) NOT NULL,
+  `motivo` int NOT NULL,
   `adjunto` blob
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
