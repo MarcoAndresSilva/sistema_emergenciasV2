ff144b470cb114321d6d24a46444bf8f5be0a927
nelson <nelsonluisnv2@gmail.com>
2024-06-12 17:27:00 -0400

    feat: update password form

---
+++ view/Perfile/index.php
@@ -0,0 +1,93 @@
+<?php
+require_once("../../config/conexion.php");
+require_once("../MainJs/js.php");
+if (isset($_SESSION["usu_id"]) && ($_SESSION["usu_tipo"] == 1 || $_SESSION["usu_tipo"] == 2)) {
+	
+	?>
+<!DOCTYPE html>
+<html>
+	<?php require_once("../MainHead/head.php") ?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-bootstrap-4/bootstrap-4.css">
+<link rel="stylesheet" href="./nivelCateogiraStyle.css">
+<title>Sistema Emergencia</title>
+<script defer type="text/javascript" src="./perfile.js"></script>
+<script src="../../public/js/sweetaler2v11-11-0.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script>
+</head>
+
+<body class="with-side-menu">
+
+    <?php require_once("../MainHeader/header.php"); ?>
+
+    <div class="mobile-menu-left-overlay"></div>
+
+    <?php require_once("../MainNav/nav.php"); ?>
+
+    <div class="page-content">
+        <div class="container-fluid">
+			<header class="section-header">
+				<div class="tbl">
+					<div class="tbl-row">
+						<div class="tbl-cell">
+							<h3>Instituciones de Emergencias</h3>
+							<ol class="breadcrumb breadcrumb-simple">
+								<li><a href="#">Registro</a></li>
+								<li class="active">Instituciones</li>
+							</ol>
+						</div>
+					</div>
+				</div>
+			</header>
+
+			<h5 class="m-t-lg with-border">Informaci&oacute;n de Instituciones de Emergencias</h5>
+    <div class="container">
+        <form id="updatePasswordForm">
+            <div class="form-group">
+                <label for="old_pass">Contraseña antigua:</label>
+                <input type="password" class="form-control" id="old_pass" name="old_pass" required>
+            </div>
+
+            <div class="form-group">
+                <label for="new_pass">Nueva contraseña:</label>
+                <input type="password" class="form-control" id="new_pass" name="new_pass" required>
+                <small id="passwordHelp" class="form-text text-muted">Debe tener al menos 8 caracteres.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="confirm_new_pass">Confirmar nueva contraseña:</label>
+                <input type="password" class="form-control" id="confirm_new_pass" name="confirm_new_pass" required>
+                <small id="confirmHelp" class="form-text"></small>
+            </div>
+
+            <input type="hidden" id="usu_id" name="usu_id" value="1"> <!-- Reemplaza el valor con el ID del usuario -->
+
+            <button type="submit" class="btn btn-primary">Cambiar contraseña</button>
+        </form>
+    </div>
+        </div><!--.container-fluid-->
+    </div><!--.page-content-->
+
+	<?php require_once("../MainFooter/footer.php"); ?>
+	
+</body>
+	<script>
+		document.getElementById('show-hide-sidebar-toggle').addEventListener('click', function(e) {
+			e.preventDefault();
+			
+			var body = document.body;
+			
+			if (!body.classList.contains('sidebar-hidden')) {
+				body.classList.add('sidebar-hidden');
+			} else {
+				body.classList.remove('sidebar-hidden');
+			}
+		});
+	</script>
+
+<?php
+
+}else{
+	header("location:".Conectar::ruta()."index.php");
+}
+?>
+</html>
---
+++ view/Perfile/perfile.js
@@ -0,0 +1,160 @@
+function fetchData(op, postData, sendAsJson = false) {
+    // URL del controlador
+    const url = '../../controller/usuario.php';
+
+    // Construir la URL con los parámetros GET
+    const params = new URLSearchParams({
+        op: op,
+    });
+
+    // Agregar los parámetros GET a la URL del controlador
+    const fetchUrl = `${url}?${params}`;
+
+    // Convertir el objeto postData a formato x-www-form-urlencoded o JSON
+    let formData;
+    let contentType;
+    if (sendAsJson) {
+        formData = JSON.stringify(postData);
+        contentType = 'application/json';
+    } else {
+        formData = new URLSearchParams(postData).toString();
+        contentType = 'application/x-www-form-urlencoded';
+    }
+
+    // Configurar la solicitud FETCH
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': contentType, // Tipo de contenido del cuerpo de la solicitud
+        },
+        body: formData, // Usar formData en lugar de JSON
+    };
+
+    // Mostrar un mensaje de carga
+    Swal.fire({
+        title: 'Cargando...',
+        allowEscapeKey: false,
+        allowOutsideClick: false,
+        onOpen: () => {
+            Swal.showLoading();
+        }
+    });
+
+    // Realizar la solicitud FETCH
+    return fetch(fetchUrl, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la solicitud.');
+            }
+            return response.json(); // Convertir la respuesta a formato JSON
+        })
+        .then(data => {
+            // Cerrar el mensaje de carga
+            Swal.close();
+
+            // Mostrar un mensaje de alerta según el estado de la respuesta
+            if (data.status === 'success') {
+                Swal.fire('Éxito', data.message, 'success');
+            } else if(data.status === 'error'){
+                Swal.fire('Error', data.message, 'error');
+            }else if(data.status === 'warning'){
+                Swal.fire('Cuidado', data.message, 'warning');
+            }else if(data.status === 'info'){
+                Swal.fire('Informacion', data.message, 'info');
+            }
+
+            return data; // Devolver la respuesta del servidor
+        })
+        .catch(error => {
+            // Cerrar el mensaje de carga
+            Swal.close();
+
+            // Mostrar un mensaje de error
+            Swal.fire('Error', 'Error al realizar la consulta.', 'error');
+
+            console.error('Error al realizar la consulta:', error);
+        });
+}
+
+document.getElementById('updatePasswordForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Recoger los datos del formulario
+    const old_pass = document.getElementById('old_pass').value;
+    const new_pass = document.getElementById('new_pass').value;
+    const confirm_new_pass = document.getElementById('confirm_new_pass').value;
+    const usu_id = document.getElementById('usu_id').value;
+
+    // Verificar si las contraseñas coinciden
+    if (new_pass !== confirm_new_pass) {
+        Swal.fire({
+            title: 'Cuidado',
+            text: "Las contraseñas no coinciden",
+            icon: 'warning',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'OK'
+        });
+        return;
+    }
+
+    // Mostrar un mensaje de confirmación
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: "¿Estás seguro de que quieres cambiar tu contraseña?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, cambiar contraseña'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Llamar a la función fetchData
+            fetchData('update_password', { old_pass: old_pass, new_pass: new_pass, usu_id: usu_id });
+        }
+    });
+});
+
+$.validator.addMethod("passwordCheck",
+    function(value, element) {
+        return this.optional(element) || /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/.test(value);
+    },
+    "La contraseña debe contener al menos una letra mayúscula, una letra minúscula y un número"
+);
+
+$(document).ready(function() {
+    $("#updatePasswordForm").validate({
+        rules: {
+            old_pass: {
+                required: true,
+            },
+            new_pass: {
+                required: true,
+                minlength: 8,
+                passwordCheck: true
+            },
+            confirm_new_pass: {
+                required: true,
+                minlength: 8,
+                equalTo: "#new_pass",
+                passwordCheck: true
+            }
+        },
+        messages: {
+            old_pass: {
+                required: "Por favor, introduce tu contraseña antigua",
+            },
+            new_pass: {
+                required: "Por favor, introduce tu nueva contraseña",
+                minlength: "Tu contraseña debe tener al menos 8 caracteres",
+                passwordCheck: "La contraseña debe contener al menos una letra mayúscula, una letra minúscula y un número"
+            },
+            confirm_new_pass: {
+                required: "Por favor, confirma tu nueva contraseña",
+                minlength: "Tu contraseña debe tener al menos 8 caracteres",
+                equalTo: "Las contraseñas no coinciden",
+                passwordCheck: "La contraseña debe contener al menos una letra mayúscula, una letra minúscula y un número"
+            }
+        }
+    });
+});
+
