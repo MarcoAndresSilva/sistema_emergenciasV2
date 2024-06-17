0e20808480a0f9b395b88539057b044c515f5929
marcosilva1088 <marco.silva@munimelipilla.cl>
2024-06-17 16:36:35 -0400

    refactor(logout) add new redirection to localhost for docker

--- view/Logout/logout.php
+++ view/Logout/logout.php
@@ -3,7 +3,9 @@ require_once("../../config/conexion.php");
 session_destroy();
 // Host
 header("Location:". "http://emergencias.melipilla.cl/");
-// Local
-header("Location:". "http://localhost/sistema_emergenciasV2/");
+// Local xammp
+//header("Location:". "http://localhost/sistema_emergenciasV2/");
+// Local docker
+header("Location:". "http://localhost/");
 exit();
 ?>
