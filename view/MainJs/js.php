5b16723b5456b66e04a3f51049ba2e9ec683e05c
marcosilva1088 <marco.silva@munimelipilla.cl>
2024-06-27 15:25:21 -0400

    refactor(resourced) "back to old libraries to JS"

--- view/MainJs/js.php
+++ view/MainJs/js.php
@@ -5,15 +5,9 @@
 <script src="../../public/js/lib/tether/tether.min.js"></script>
 <!-- Bootstrap -->
 <script src="../../public/js/lib/bootstrap/bootstrap.min.js"></script>
-
 <!-- bootstrap-sweetalert -->
 <script src="../../public/js/lib/bootstrap-sweetalert/sweetalert.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
-<script src="../../public/js/sweetaler2v11-11-0.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
 
 <script src="../../public/js/plugins.js"></script>
 <script src="../../public/js/app.js"></script>
