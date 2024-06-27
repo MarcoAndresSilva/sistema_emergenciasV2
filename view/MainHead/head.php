cab57479f753cc76f5860ef1e4ad573122fa4218
marcosilva1088 <marco.silva@munimelipilla.cl>
2024-06-27 13:02:02 -0400

    refactor(resourced) "add new libraries to CSS"

--- view/MainHead/head.php
+++ view/MainHead/head.php
@@ -3,6 +3,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-bootstrap-4/bootstrap-4.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/  bootstrap.min.css" rel="stylesheet">
+
 <link rel="icon" href="../../public/img/logo-meli-sin-nombre.png">
 <link rel="stylesheet" href="../../public/css/separate/vendor/fancybox.min.css">
 <link rel="stylesheet" href="../../public/css/separate/pages/activity.min.css">
