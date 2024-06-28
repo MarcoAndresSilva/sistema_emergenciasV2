73f8c4a83db99404b1efa8f26e62dbc1d9d221ac
marcosilva1088 <marco.silva@munimelipilla.cl>
2024-06-28 11:22:53 -0400

    feat(view) "add new php files to concret the functionality"

---
+++ view/ControlEventosDos/modalCerrar.php
@@ -0,0 +1,21 @@
+
+<!-- Modal Derivar -->
+<div class="modal fade" id="modalCerrar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="miTitle">Panel de Cierre</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <!-- Form content -->
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Derivar</button>
+      </div>
+    </div>
+  </div>
+</div>
