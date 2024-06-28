2ec4c8120a7706c7041d0074f863384987076971
marcosilva1088 <marco.silva@munimelipilla.cl>
2024-06-28 11:24:07 -0400

    fix(head)"add and orden new dependencies project"

--- view/MainHead/head.php
+++ view/MainHead/head.php
@@ -4,35 +4,30 @@
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 
 <link rel="icon" href="../../public/img/logo-meli-sin-nombre.png">
+
+<link rel="stylesheet" href="../../public/css/lib/bootstrap/bootstrap.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<link rel="stylesheet" href="../../public/css/main.css">
+
+<link rel="stylesheet" href="../../public/css/lib/font-awesome/font-awesome.min.css">
+
 <link rel="stylesheet" href="../../public/css/separate/vendor/fancybox.min.css">
 <link rel="stylesheet" href="../../public/css/separate/pages/activity.min.css">
 
-<link rel="stylesheet" href="../../public/css/lib/summernote/summernote.css"/>
 <link rel="stylesheet" href="../../public/css/separate/pages/editor.min.css">
+<link rel="stylesheet" href="../../public/css/separate/pages/login.min.css">
 
-<link rel="stylesheet" href="../../public/css/lib/font-awesome/font-awesome.min.css">
-<link rel="stylesheet" href="../../public/css/lib/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" href="../../public/css/separate/vendor/select2.min.css">
+<link rel="stylesheet" href="../../public/css/separate/vendor/bootstrap-select/bootstrap-select.min.css">
+
+<link rel="stylesheet" href="../../public/css/lib/summernote/summernote.css"/>
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
 
 <link rel="stylesheet" href="../../public/css/lib/bootstrap-sweetalert/sweetalert.css">
 <link rel="stylesheet" href="../../public/css/separate/vendor/sweet-alert-animations.min.css">
 
 <link rel="stylesheet" href="../../public/css/lib/datatables-net/datatables.min.css">
 <link rel="stylesheet" href="../../public/css/separate/vendor/datatables-net.min.css">
-
-<link rel="stylesheet" href="../../public/css/separate/vendor/select2.min.css">
-<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-
-<link rel="stylesheet" href="../../public/css/main.css">
-
-<link rel="stylesheet" href="../../public/css/lib/summernote/summernote.css"/>
-<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
-
-<link rel="stylesheet" href="../../public/css/separate/pages/editor.min.css">
-<link rel="stylesheet" href="../../public/css/separate/pages/login.min.css">
-
-<link rel="stylesheet" href="../../public/css/lib/font-awesome/font-awesome.min.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<link rel="stylesheet" href="../../public/css/main.css">
 
 <link rel="stylesheet" href="../MainNav/estilopersonal.css">
 <link rel="javascript" href="../MainNav/nav.js">
