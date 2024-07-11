6cc3bb51bbb36c658f509560ebe27976e51c0573
Nelson Navarro Valenzula <nelson.navarro@munimelipilla.cl>
2024-07-11 10:18:02 -0400

    style: organize code

--- view/GestionUsuario/index.php
+++ view/GestionUsuario/index.php
@@ -2,8 +2,8 @@
 require_once("../../config/conexion.php");
 require_once("../MainJs/js.php");
 if (isset($_SESSION["usu_id"]) && ($_SESSION["usu_tipo"] == 1 || $_SESSION["usu_tipo"] == 2)) {
-	
-	?>
+    
+    ?>
 <!DOCTYPE html>
 <html>
 	<?php require_once("../MainHead/head.php") ?>
@@ -12,9 +12,13 @@ if (isset($_SESSION["usu_id"]) && ($_SESSION["usu_tipo"] == 1 || $_SESSION["usu_
 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.2/css/responsive.bootstrap5.css"> 
 <link rel="stylesheet" href="./gestionUsuario.css">
 <title>Sistema Emergencia</title>
-<script defer type="text/javascript" src="./GestionPerfile.js"></script>
 <script src="../../public/js/sweetaler2v11-11-0.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script>
+<script defer src="https://cdn.datatables.net/2.0.7/js/dataTables.js"></script>
+<script defer src="https://cdn.datatables.net/2.0.7/js/dataTables.bootstrap5.js"></script>
+<script defer src="https://cdn.datatables.net/responsive/3.0.2/js/dataTables.responsive.min.js"></script>
+<script defer src="https://cdn.datatables.net/responsive/3.0.2/js/responsive.dataTables.min.js"></script>
+<script defer src="./GestionPerfile.js"></script>
 </head>
 
 <body class="with-side-menu">
@@ -36,50 +40,41 @@ if (isset($_SESSION["usu_id"]) && ($_SESSION["usu_tipo"] == 1 || $_SESSION["usu_
 								<li><a href="#">Registro</a></li>
 								<li class="active">Instituciones</li>
 							</ol>
+              </div>
 						</div>
 					</div>
+				</header>
+				<h5 class="m-t-lg with-border">Informaci&oacute;n de Instituciones de Emergencias</h5>
+				<div class="container">
+				    <h2>Información del Usuario</h2>
+				    <a href="../CrearUsuario/" class="btn btn-success">Crear usuario</a>
+				    <div id="userInfo" class="row justify-content-center">
+				        <div class="spinner-border text-primary mt-5" role="status">
+				            <span class="sr-only">Cargando...</span>
+				        </div>
+				    </div>
 				</div>
-			</header>
+	        </div><!--.container-fluid-->
+	    </div><!--.page-content-->
+		<?php require_once("../MainFooter/footer.php"); ?>
 
-			<h5 class="m-t-lg with-border">Informaci&oacute;n de Instituciones de Emergencias</h5>
-
-<div class="container">
-    <h2>Información del Usuario</h2>
-    <a href="../CrearUsuario/" class="btn btn-success">Crear usuario</a>
-    <div id="userInfo" class="row justify-content-center">
-        <div class="spinner-border text-primary mt-5" role="status">
-            <span class="sr-only">Cargando...</span>
-        </div>
-    </div>
-</div>
-        </div><!--.container-fluid-->
-    </div><!--.page-content-->
-
-	<?php require_once("../MainFooter/footer.php"); ?>
-	
-</body>
-	<script>
-		document.getElementById('show-hide-sidebar-toggle').addEventListener('click', function(e) {
-			e.preventDefault();
-			
-			var body = document.body;
-			
-			if (!body.classList.contains('sidebar-hidden')) {
-				body.classList.add('sidebar-hidden');
-			} else {
-				body.classList.remove('sidebar-hidden');
-			}
-		});
-	</script>
-
+		<script>
+			document.getElementById('show-hide-sidebar-toggle').addEventListener('click', function(e) {
+				e.preventDefault();
+				
+				var body = document.body;
+				
+				if (!body.classList.contains('sidebar-hidden')) {
+					body.classList.add('sidebar-hidden');
+				} else {
+					body.classList.remove('sidebar-hidden');
+				}
+			});
+		</script>
+	</body>
+</html>
 <?php
-echo ' <script  src="https://cdn.datatables.net/2.0.7/js/dataTables.js"></script>';
-echo '<script defer src="https://cdn.datatables.net/2.0.7/js/dataTables.bootstrap5.js"></script>';
-echo '<script defer src=" https://cdn.datatables.net/responsive/3.0.2/js/dataTables.responsive.js"></script>';
-echo '<script defer src="https://cdn.datatables.net/responsive/3.0.2/js/responsive.datatables.js"></script>';
- 
 }else{
 	header("location:".Conectar::ruta()."index.php");
 }
 ?>
-</html>
