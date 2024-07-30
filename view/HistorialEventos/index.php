5c739828165348bfc21a90370ac65bee9c747db3
marcosilva1088 <marco.silva@munimelipilla.cl>
2024-07-30 15:57:49 -0400

    feat(html) new index associated the new view of historialEventos

---
+++ view/HistorialEventos/index.php
@@ -0,0 +1,102 @@
+<?php
+require_once("../../config/conexion.php");
+if (isset($_SESSION["usu_id"]) && ($_SESSION["usu_tipo"] == 1 || $_SESSION["usu_tipo"] == 2)) {
+?>
+
+<!DOCTYPE html>
+<html>
+    <?php require_once("../MainHead/head.php") ?>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-bootstrap-4/bootstrap-4.css">
+    <link rel="stylesheet" href="./historialeventos.css">
+    <script src="../../public/js/sweetaler2v11-11-0.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    <title>Sistema Emergencia</title>
+</head>
+
+<body class="with-side-menu">
+
+    <?php require_once("../MainHeader/header.php"); ?>
+
+    <div class="mobile-menu-left-overlay"></div>
+
+    <?php require_once("../MainNav/nav.php"); ?>
+
+    <div class="page-content">
+        <div class="container-fluid">
+            <header class="section-header">
+                <div class="tbl">
+                    <div class="tbl-row">
+                        <div class="tbl-cell">
+                            <h3>Historial de las Emergencias</h3>
+                            <ol class="breadcrumb breadcrumb-simple">
+                                <li><a href="#">Emergencias</a></li>
+                                <li class="active">Detalle de las Emergencias</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            
+            <h5 class="m-t-lg with-border">Informaci&oacute;n Actual</h5>
+            <div class="box-typical box-typical-padding table table-responsive-sm">
+
+                <!-- tabla de asuntos inmediatos (Alto) -->
+                <table class="table tabla-media tabla-basica table-bordered table-striped table-vcenter js-dataTable-js">
+                    <thead>
+                        <tr>
+                            <th style="width:5%">ID</th>
+                            <th style="width:10%">Categor&iacute;a</th>
+                            <th style="width:10%">Direcci&oacute;n</th>
+                            <th style="width:25%">Asignaci&oacute;n</th>
+                            <th style="width:10%">Nivel Peligro</th>
+                            <th style="width:10%">Estado</th>
+                            <th style="width:15%">Fecha Apertura</th>
+                            <th style="width:5%">Ver Detalle</th>
+                        </tr>
+                    </thead>
+                    <tbody id="datos-criticos">
+                        <!-- Datos de consulta -->
+                    </tbody>
+                    <tbody id="datos-medios">
+                        <!-- Datos de consulta -->
+                    </tbody>
+                    <tbody id="datos-bajos">
+                        <!-- Datos de consulta -->
+                    </tbody>
+                    <tbody id="datos-generales">
+                        <!-- Datos de consulta -->
+                    </tbody>
+                </table>
+            </div>
+
+            
+            <div id="modal-mapa" class="modal-overlay">
+                <div class="vista-mapa" id="map">
+                </div>
+                <button id='btn' type='button' class='btn btn-inline btn-primary btn-sm ladda-button btnCrearRuta' > Ir </button>
+                <span class="glyphicon glyphicon-remove CerrarModalMap"></span>
+            </div>
+
+
+
+        </div>
+    </div><!--.page-content-->
+
+    <?php require_once("../MainFooter/footer.php"); ?>
+
+    <?php
+
+require_once("../MainJs/js.php");
+?>
+</body>
+
+<script type="text/javascript" src="./historialeventos.js"></script>
+</html>
+
+<?php
+} else {
+    header("location:" . Conectar::ruta() . "index.php");
+}
+?>
