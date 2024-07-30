e35cfec3cede841431bc16e8811e1d75943b17f9
NelsonNV <nelsonluisnv2@gmail.com>
2024-07-30 12:48:34 -0400

    refactor: the colors are now not random, they are based on the name

--- view/MapaCalor/mapacalor.js
+++ view/MapaCalor/mapacalor.js
@@ -39,7 +39,7 @@ function initMap() {
 function addHeatmapLayers(categories) {
   Object.keys(categories).forEach(category => {
     if (!categoryColors[category]) {
-      categoryColors[category] = getRandomColor();
+      categoryColors[category] = generateColorFromCategory(category);
     }
 
     const points = categories[category].map(item => new google.maps.LatLng(item.latitud, item.longitud));
@@ -254,11 +254,15 @@ function createIcon(iconClass) {
   return icon;
 }
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
+function generateColorFromCategory(category) {
+  let hash = 0;
+  for (let i = 0; i < category.length; i++) {
+    hash = category.charCodeAt(i) + ((hash << 5) - hash);
+  }
   let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+  for (let i = 0; i < 3; i++) {
+    let value = (hash >> (i * 8)) & 0xFF;
+    color += ('00' + value.toString(16)).slice(-2);
   }
   return color;
 }
