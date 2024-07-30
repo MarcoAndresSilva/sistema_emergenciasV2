137553b1e3defc8d6ab4c34d5aad4ad34c76d167
marcosilva1088 <marco.silva@munimelipilla.cl>
2024-07-30 15:56:59 -0400

    feat(js) new js associated the new view of historialEventos

---
+++ view/HistorialEventos/historialeventos.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    // Obtener el elemento <a> por su ID
+    var enlace = document.querySelector('.HistorialEventos');
+    // Añadir una clase al enlace
+    enlace.classList.add('selected');
+
+    cargarTablaGeneral();
+});
+
+function cargarTablaGeneral() {
+    // Funcion para cargar los datos de las tablas
+    $.post("../../controller/eventoDos.php?op=tabla-historial-eventos", function(respuesta, status) {
+        // Parsear la respuesta JSON
+        var data = JSON.parse(respuesta);
+        
+        $('#datos-criticos').html(data.critico);
+        $('#datos-medios').html(data.medio);
+        $('#datos-bajos').html(data.bajo);
+        $('#datos-generales').html(data.comun);
+
+        // Agregar el evento click a los botones de ver detalle
+        $('.btnDetalleEmergencia').click(function() {
+            var ev_id = $(this).data('ev-id');
+            ver(ev_id);
+        });
+    });
+}
+
+function ver(ev_id) {
+    // Abrir una nueva pestaña con la ruta especificada
+    window.open(`../EmergenciaDetalle?ID=${ev_id}`, '_blank');
+}
+
