7eb794e0f63a0a6e68d6cacf92083eb88cc6f0b4
marcosilva1088 <marco.silva@munimelipilla.cl>
2024-08-13 12:26:39 -0400

    feat(view) add new functionality Add new functionality to the map: now I can see point markers on the map and create a route to emergency events.

--- view/HistorialEventos/historialeventos.js
+++ view/HistorialEventos/historialeventos.js
@@ -32,3 +32,107 @@ function ver(ev_id) {
     window.open(`../EmergenciaDetalle?ID=${ev_id}`, '_blank');
 }
 
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+let lat;
+let long;
+$(document).on('click', '.btnDireccionarMapa',function() {
+    
+    //Desplegar mapa para direccionar al lugar
+    toggleMapa();
+    
+    // Obtener el valor del ID del evento desde la celda
+    ev_id = $(this).closest('tr').find('#id_evento_celda').attr('value');
+    // ev_id = 85;
+    consultarEventoMostarMapa(ev_id);
+});
+
+function toggleMapa() {   
+    $('#modal-mapa').toggle();
+}
+
+function consultarEventoMostarMapa(ev_id) {
+
+    $.post("../../controller/evento.php?op=get_evento_id",{ev_id : ev_id}, function(data,status){
+        
+        var eventos = JSON.parse(data);
+
+        lat = eventos[0]['ev_latitud'];
+        long = eventos[0]['ev_longitud'];
+
+        mostrarMapa(lat,long);
+    });
+}
+
+var LocationUserOrigin;
+
+function mostrarMapa(lat,long) {
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17 // Nivel de zoom
+    });
+
+    //Activación y ejecución de la obtencion de coordenadas del usuario
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+                
+            var userLocation = {
+                lat: lat,
+                lng: long
+            };
+            LocationUserOrigin = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            //Marcador que ingresa el usuario
+            marker = new google.maps.Marker({
+                position: userLocation, // Coordenadas del marcador
+                map: map,
+                title: 'ArrastrarEmergencia'
+            });
+            map.setCenter(userLocation);
+
+        }, function(error) {        
+            // Manejo de errores
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    console.error("El usuario denegó la solicitud de geolocalización.");
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    console.error("La información de ubicación no está disponible.");
+                    break;
+                case error.TIMEOUT:
+                    console.error("Se agotó el tiempo de espera para la solicitud de geolocalización.");
+                    break;
+                    default:
+                        console.error("Error desconocido al intentar obtener la ubicación.");
+            }
+            swal("Error de Geolocalización!","No se logro optener la ubicación", "error");
+        });
+    } else {
+        console.error('Error: El navegador no soporta geolocalización.');
+    }
+}
+
+//Btn Crear ruta)
+$('.btnCrearRuta').off('click').on('click',function(){
+    
+    // Redirecciona a google maps
+    window.location.href = "https://www.google.com/maps/dir/" + LocationUserOrigin.lat + "," + LocationUserOrigin.lng + "/" + lat + "," + long  ;
+
+});
+
+$('.CerrarModalMap').off('click').on('click',function(){
+    
+    // Llamar a la función para mostrar u ocultar la pestaña
+    toggleMapa();
+
+});
+
+// Este script debe estar después de incluir la API de Google Maps
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644}, // Coordenadas iniciales
+        zoom: 8 // Nivel de zoom
+    });
+}
