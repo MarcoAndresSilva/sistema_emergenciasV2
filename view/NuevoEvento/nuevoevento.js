f8dbe692df1233e6a75f15a0edf79e6b1b2698af
NelsonNV <nelsonluisnv2@gmail.com>
2024-08-14 12:15:32 -0400

    feat: clear form

--- view/NuevoEvento/nuevoevento.js
+++ view/NuevoEvento/nuevoevento.js
@@ -319,6 +319,8 @@ async function add_evento() {
       if (data.status === 'success') {
         swal("Éxito", data.message, "success")
           addNotification("Nuevo Evento", "evento nuevo por derivar", "../ControlEventos/");
+          formulario = document.getElementById("event_form");
+          formulario.reset();
       } else if (data.status === 'error') {
         swal("Error", data.message, "error");
       } else {
