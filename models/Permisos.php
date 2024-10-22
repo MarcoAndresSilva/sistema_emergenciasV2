0673bf7fcbaeac7b1713dffc2a8d4a0eeef43749
NelsonNV <nelsonluisnv2@gmail.com>
2024-10-22 12:18:38 -0300

    refactor: verify login

--- models/Permisos.php
+++ models/Permisos.php
@@ -1,15 +1,17 @@
 <?php
 require_once(__DIR__."/../config/conexion.php");
 class Permisos extends Conectar{
+  public static function verificarLogin() {
         try {
-            return isset($_SESSION["usu_id"]) && ($_SESSION["usu_tipo"] == 1 || $_SESSION["usu_tipo"] == 2);
+            return !empty($_SESSION["usu_id"]) && !empty($_SESSION["usu_tipo"]);
         } catch (Exception $e) {
             // Puedes registrar el error si es necesario
             error_log($e->getMessage());
             return false;
         }
-    }    public static function redirigirSiNoAutorizado() {
-        if (!self::verificarPermisos()) {
+  }
+  public static function redirigirSiNoAutorizado() {
+        if (!self::verificarLogin()) {
             header("Location: " . Conectar::ruta() . "index.php");
             exit();
         }
