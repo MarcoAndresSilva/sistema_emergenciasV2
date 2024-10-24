c448c30a244131aa028090ff7a9ed7e08161f5c1
marcosilva1088 <marco.silva@munimelipilla.cl>
2024-10-24 16:53:37 -0300

    feat(modal.php): update modal view for unit selection
    Updated the modal to display a new view that shows available units and
    their sections for selection, allowing delegation to each selected unit.
    Added the Multiselect library and changed how the ID and category
    information are displayed.

--- view/ControlEventos/modalDerivar.php
+++ view/ControlEventos/modalDerivar.php
@@ -1,57 +1,67 @@
 
-<!-- Modal Derivar -->
 <div class="modal fade" id="modalDerivar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="miTitle">Panel de Derivaciones</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form>
-          <div id="distribucion-box">
-            <div id="distribucion-espacios">
-              <fieldset class="form-group" id="grupito">
-                <label class="form-label bold" for="id_nivel_peligro" id="asignaciones-title">Id de Evento</label>
-                <div id="ev_id"></div>
-              </fieldset>
-              <fieldset class="form-group" id="grupito">
-                <label class="form-label bold" for="id_nivel_peligro" id="asignaciones-title">Categoria</label>
-                <div id="cat_nombre_derivar"></div>
-              </fieldset>
-              <fieldset class="form-group">
-                <label class="form-label bold" for="id_nivel_peligro" id="asignaciones-title">Nivel de Peligro </label>
-                <select id="niv_id" class="form-control" >
-                  <!-- Datos de consulta -->
-                </select>
-              </fieldset>
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="miTitle">Panel de Derivaciones</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div id="distribucion-espacios">
-              <fieldset class="form-group">
-                <label class="form-label bold" id="asignaciones-title">Asignar Unidad</label>
-                <div id="unidadOptions" class="form-check">
-                  <!-- Las opciones se agregarán aquí dinámicamente -->
-                </div>
-              </fieldset>
+            <div class="modal-body">
+                <!-- Dependencias CSS -->
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-bootstrap-4/bootstrap-4.css">
+                <link rel="stylesheet" href="./estilopersonaleventos.css">
 
-              <fieldset class="form-group box-selection" id="buttons-group">
-                <div class="btn-box">
-                  <button id="btn"type='button' class='btn btn-inline btn-success btn-sm ladda-button btnActualizarTodos'> Derivar
-                    <span><i class="fa-solid fa-arrow-up-from-bracket"></i> </span>
-                  </button>		
-                </div>
-              </fieldset>	
+                <form>
+                    <div id="distribucion-box">
+
+                        <div id="distribucion-espacios-datos">      
+                            <div class="card">
+                                <div class="card-body"> 
+                                    <h6 class="card-subtitle mb-2 text-muted">ID</h6>
+                                    <div id="derivar_ev_id"></div>
+                                </div>
+                            </div>
+                             <div class="card">
+                                <div class="card-body"> 
+                                    <h6 class="card-subtitle mb-2 text-muted">CATEGORIA</h6>
+                                    <div id="derivar_cat_nombre"></div>
+                                </div>
+                            </div> 
+                        </div>        
+
+                        <div id="distribucion-espacios">
+                             <div class="card">
+                                <div class="card-body"> 
+                                    <h6 class="card-subtitle mb-2 text-muted">SECCIONES</h6>
+                                    <select id="multipleSelect" multiple name="native-select" placeholder="Native Select" data-search="false" data-silent-initial-value-set="true">
+                                    </select> 
+                                </div>
+                             </div>
+                            <fieldset class="form-group box-selection" id="buttons-group">
+                                <div class="btn-box">
+                                    <button id="btn" type='button' class='btn btn-inline btn-success btn-sm ladda-button btnActualizarTodos'>
+                                        Derivar
+                                        <span><i class="fa-solid fa-arrow-up-from-bracket"></i></span>
+                                    </button>		
+                                </div>
+                            </fieldset>	
+                        </div>
+                    </div>
+                </form>
             </div>
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-      <!-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button> -->
-      <!-- <button type="button" class="btn btn-primary">Derivar</button> -->
-      </div>
+            <div class="modal-footer">
+                <!-- Puedes agregar botones adicionales aquí si es necesario -->
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
+<!-- Dependencias JavaScript -->
+<script src="../../public/js/sweetaler2v11-11-0.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="./derivar.js"></script>
+<script type="text/javascript" src="./evento.js"></script>
 
 
