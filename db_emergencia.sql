23d405609d97c16a7174786754f5733beccdaceb
NelsonNV <nelsonluisnv2@gmail.com>
2024-11-05 12:41:19 -0300

    refactor(db): more section tests at startup
    sections are renamed and assigned to more units
    
    also create and assign more users to those units

--- db_emergencia.sql
+++ db_emergencia.sql
@@ -374,9 +374,9 @@ CREATE TABLE IF NOT EXISTS `tm_seccion` (
 
 INSERT INTO `tm_seccion` (`sec_id`, `sec_nombre`, `sec_detalle`, `sec_unidad`) VALUES
 (1, 'grupo1', 'Administracion de la aplicacion', '1'),
-(2, 'grupo2', 'Administracion de usuarios', '1'),
-(3, 'grupo3', 'Desarrollo de la aplicacion', '5'),
-(4, 'grupo4', 'Desarrollo de la aplicacion', '5'),
+(2, 'grupo2', 'Administracion de usuarios', '2'),
+(3, 'grupo3', 'Administrar Evento', '3'),
+(4, 'grupo4', 'Encargados de tramposte', '4'),
 (5, 'grupo5', 'Desarrollo de la aplicacion', '5');
 
 CREATE TABLE `tm_usuario` (
@@ -401,10 +401,12 @@ CREATE TABLE `tm_usuario` (
 --
 
 INSERT INTO `tm_usuario` (`usu_id`, `usu_nom`, `usu_ape`, `usu_correo`, `usu_telefono`, `usu_name`, `usu_pass`, `fecha_crea`, `fecha_modi`, `fecha_elim`, `estado`, `usu_tipo`, `usu_unidad`, `usu_seccion`) VALUES
-(1, 'Cristian', 'Suazo', 'cristian.suazo@munimelipilla.cl', 968093527, 'csuazo', 'e10adc3949ba60abbe56e057f20f883e', '2023-12-30 13:15:58', NULL, NULL, 1, 2,5,3),
-(2, 'Admin', 'admin', 'admin@mail.com', 961718297, 'admin', md5('123'), '2024-01-03 18:09:32', NULL, NULL, 1, 2,5,2),
-(3, 'Marco', 'Silva', 'marco.silva@munimelipilla.cl', 997827161, 'msilva', '827ccb0eea8a706c4c34a16891f84e7b', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 3,5,5),
-(4, 'Nelson', 'Navarro', 'nelson.navarro@munimelipilla.cl', 965412724, 'nnavarro', 'b08cab3a853b8d1e609905d7161daf80', '2024-03-20 13:01:58', NULL, NULL, 1, 3,5,5);
+(1, 'Cristian', 'Suazo', 'cristian.suazo@munimelipilla.cl', 968093527, 'csuazo', 'e10adc3949ba60abbe56e057f20f883e', '2023-12-30 13:15:58', NULL, NULL, 1, 2,1,3),
+(2, 'Admin', 'admin', 'admin@mail.com', 961718297, 'admin', md5('123'), '2024-01-03 18:09:32', NULL, NULL, 1, 2,5,5),
+(3, 'Marco', 'Silva', 'marco.silva@munimelipilla.cl', 997827161, 'msilva', '827ccb0eea8a706c4c34a16891f84e7b', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 3,3,5),
+(4, 'Nelson', 'Navarro', 'nelson.navarro@munimelipilla.cl', 965412724, 'nnavarro', 'b08cab3a853b8d1e609905d7161daf80', '2024-03-20 13:01:58', NULL, NULL, 1, 3,4,5),
+(5, 'Dangelo', 'Contreras', 'dangelo.contreras@munimelipilla.cl', 965412724, 'dcontreras',  md5('Dcontrer!123') , '2024-03-20 13:01:58', NULL, NULL, 1, 3,2,2),
+(6, 'Jose', 'Ruminot', 'joseluis.ruminot@munimelipilla.cl', 965412724, 'JRumino',  md5('JRumino!1234') , '2024-03-20 13:01:58', NULL, NULL, 1, 3,2,2);
 
 
 
