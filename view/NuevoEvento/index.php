7ec5b5416bf2cf5c5b8c83444337374131570438
marcosilva1088 <marco.silva@munimelipilla.cl>
2024-12-09 13:18:42 -0300

    style(css): reform input form design and adjust media queries
    Updated the input form layout with a new design, including enhanced
    input field styles. Adjusted media queries to ensure compatibility and
    responsiveness across all screen resolutions.

--- view/NuevoEvento/index.php
+++ view/NuevoEvento/index.php
@@ -38,87 +38,88 @@ if (isset($_SESSION["usu_id"])) {
                 </div>
             </header>
 
-            <h5 class="m-t-lg with-border">Ingresar información</h5>
 
+            <h5 class="m-t-lg with-border" id="subtitle">Información de usuario</h5>
             <div class="row">
-                 <div class="col-lg-6 col-md-6 mb-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <h6 class="card-subtitle mb-2 text-muted">Nombre</h6>
-                            <p class="card-text"><?php echo $_SESSION['usu_nom']; ?></p>
+                <div class="data-user">
+                    <div class="col-lg-6 col-md-6 mb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h6 class="card-subtitle mb-2 text-muted">Nombre</h6>
+                                <p class="card-text"><?php echo $_SESSION['usu_nom']; ?></p>
+                            </div>
+                        </div>
+                     </div>
+                    <div class="col-lg-6 col-md-6 mb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h6 class="card-subtitle mb-2 text-muted">Apellido</h6>
+                                <p class="card-text"><?php echo $_SESSION['usu_ape']; ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 mb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h6 class="card-subtitle mb-2 text-muted">Teléfono</h6>
+                                <p class="card-text"><?php echo $_SESSION['usu_telefono']; ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 mb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h6 class="card-subtitle mb-2 text-muted">Dirección de correo electrónico</h6>
+                                <p class="card-text"><?php echo $_SESSION['usu_correo']; ?></p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-6 mb-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <h6 class="card-subtitle mb-2 text-muted">Apellido</h6>
-                            <p class="card-text"><?php echo $_SESSION['usu_ape']; ?></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-6 mb-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <h6 class="card-subtitle mb-2 text-muted">Teléfono</h6>
-                            <p class="card-text"><?php echo $_SESSION['usu_telefono']; ?></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-6 mb-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <h6 class="card-subtitle mb-2 text-muted">Dirección de correo electrónico</h6>
-                            <p class="card-text"><?php echo $_SESSION['usu_correo']; ?></p>
-                        </div>
-                    </div>
-                </div>
+                
+                <h5 class="m-t-lg with-border" id="subtitle">Ingresar información del evento</h5>
                 <form class="event-box" action="nuevoevento.js" method="post" id="event_form" enctype="multipart/form-data">
-                    <div class="group-emergencia">
-                    <div class="col-lg-5">
-                        <fieldset class="form-group">
-                            <label class="form-label semibold" for="exampleInput">Seleccione una Categoría</label>
-                            <select id="cat_id" class="form-control">
-                          <!-- Datos recopilados desde función get_categoria -->
-                        </select>
-                      </fieldset>
+                    <div class="group-emergencia-data">
+                        <div class="col-lg-5 col-cat">
+                            <fieldset class="form-group">
+                                <label class="form-label semibold" for="exampleInput">Seleccione Categoría</label>
+                                <select id="cat_id" class="form-control"></select>
+                            </fieldset>
+                        </div>
+                        <div class="col-lg-5 col-img">
+                            <fieldset class="form-group">
+                                <label class="form-label semibold" for="formFile">Desea adjuntar una imagen</label>
+                                <input type="file" class="form-control" id="imagen" name="imagen" accept="image/*">
+                                <small id="archivoAdjuntado" class="form-text text-muted">No hay archivo adjunto (.JPG/.JPEG/.PNG)</small>
+                            </fieldset>
+                        </div>
+                        <div class="col-lg-5 col-des">            
+                            <fieldset class="form-group">
+                                <label class="form-label semibold" for="descripcion">Ingrese una breve descripción</label>
+                                <input type="text" class="form-control" id="descripcion" placeholder="Descripción">
+                            </fieldset>
+                        </div>
                     </div>
-                <div class="col-lg-5">
-                      <fieldset class="form-group">
-                        <label class="form-label semibold" for="descripcion">Ingrese una breve descripción</label>
-                        <input type="text" class="form-control" id="descripcion" placeholder="Descripción">
-                      </fieldset>
-                    </div>
-                    <div class="col-lg-5">
-                      <fieldset class="form-group">
-                        <label class="form-label semibold" for="formFile">Desea adjuntar una imagen de la emergencia?</label>
-                        <input type="file" class="form-control" id="imagen" name="imagen" accept="image/*">
-                        <small id="archivoAdjuntado" class="form-text text-muted">No hay archivo adjunto (.JPG/.JPEG/.PNG)</small>
-                      </fieldset>
-                    </div>
-                  </div>
 
-                  <div class="group-ident">
-                    <div class="col-lg-5">
-                      <fieldset class="form-group">
-                        <label class="form-label semibold" for="exampleInput">Ingresar dirección o Marque en el mapa</label>
-                        <input type="text" class="form-control" id="address" placeholder="Dirección">
-                      </fieldset>
+                    <div class="group-ident">
+                        <div class="col-lg-5">
+                            <fieldset class="form-group group-dir">
+                                <label class="form-label semibold" for="exampleInput">Ingresar dirección o Marque en el mapa</label>
+                                <input type="text" class="form-control" id="address" placeholder="Dirección">
+                            </fieldset>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="solicitarUbicacion">
+                                <label class="form-check-label" for="solicitarUbicacion">Solicitar mi ubicación actual</label>
+                            </div>
+                        </div> 
                     </div>
-                    <div class="col-lg-8 d-flex align-items-center">
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="solicitarUbicacion">
-                        <label class="form-check-label" for="solicitarUbicacion">Solicitar mi ubicación actual</label>
-                      </div>
+                    <div class="mapa">
+                        <div id="map" style="height: 400px; width: 100%;"></div>
+                        <input type="hidden" id="ev_latitud">
+                        <input type="hidden" id="ev_longitud">
                     </div>
-                    <div id="map" style="height: 400px; width: 100%;"></div>
-                    <input type="hidden" id="ev_latitud">
-                    <input type="hidden" id="ev_longitud">
-                  </div>
-
-                  <div class="col-lg-10">
-                    <button type="button" class="btn btn-round btn-inline btn-primary" id="btnGuardar">AGREGAR NUEVA EMERGENCIA</button>
-                  </div>
+                    <div class="col-lg-10">
+                        <button type="button" class="btn btn-round btn-inline btn-primary" id="btnGuardar">AGREGAR NUEVA EMERGENCIA</button>
+                    </div>
                 </form>               
             </div>
         </div><!--.container-fluid-->
